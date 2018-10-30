9719e0e826ac935ed9842459ddeab17afe57f9d7
Stephen De Vight <devights@gmail.com>
2018-10-30 16:33:23 -0700

    setup

---
+++ setup.py
@@ -0,0 +1,39 @@
+import os
+from setuptools import setup
+
+README = """
+See the README on `GitHub
+<https://github.com/uw-it-aca/prereq_map>`_.
+"""
+
+# The VERSION file is created by travis-ci, based on the tag name
+version_path = 'prereq_map/VERSION'
+VERSION = open(os.path.join(os.path.dirname(__file__), version_path)).read()
+VERSION = VERSION.replace("\n", "")
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+url = "https://github.com/uw-it-aca/myuw"
+setup(
+    name='Prerequisite Map',
+    version=VERSION,
+    packages=['prereq_map'],
+    author="UW-IT AXDD",
+    author_email="aca-it@uw.edu",
+    include_package_data=True,
+    install_requires=[
+        'Django>=2.0',
+    ],
+    license='Apache License, Version 2.0',
+    description='A tool for visually displaying UW course prerequisites',
+    long_description=README,
+    url=url,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
