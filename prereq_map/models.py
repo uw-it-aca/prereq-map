ac1ac33fd7c4788a456ac3e43a62e84c72c12e48
Stephen De Vight <devights@gmail.com>
2018-11-09 10:48:29 -0800

    fixing models

--- prereq_map/models.py
+++ prereq_map/models.py
@@ -3,14 +3,14 @@ from django.db import models
 
 class Prerequisite(models.Model):
     course_branch = models.SmallIntegerField()
-    department_abbrev = models.CharField()
+    department_abbrev = models.CharField(max_length=32)
     course_number = models.SmallIntegerField()
     last_eff_year = models.SmallIntegerField()
-    last_eff_qtr = models.CharField()
+    last_eff_qtr = models.CharField(max_length=6)
     pr_seq_no = models.SmallIntegerField()
-    pr_curric_abbr = models.CharField()
+    pr_curric_abbr = models.CharField(max_length=32)
     pr_course_no = models.SmallIntegerField()
     pr_group_no = models.SmallIntegerField()
-    pr_and_or = models.CharField()
+    pr_and_or = models.CharField(max_length=3)
     pr_cr_s = models.BooleanField()
     pr_concurrency = models.BooleanField()
