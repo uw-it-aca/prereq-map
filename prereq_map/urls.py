a65e660f300e4a2d8126c4d5e5e5efaee85000e6
Charlon Palacay <charlon@gmail.com>
2019-02-26 10:31:09 -0800

    Updates urls.py

--- prereq_map/urls.py
+++ prereq_map/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', RedirectView.as_view(url='/curriculum-search/')),
     path('curriculum-search/', CurriculumSearch.as_view()),
     path('course-search/', CourseSearch.as_view()),
-    re_path('api/course/(?P<course_code>.*)', CourseApiView.as_view()),
-    re_path('api/curric/(?P<curric_code>.*)', CurricApiView.as_view())
+    re_path(r'^api/curric/(?P<curric_code>.*)', CurricApiView.as_view()),
+    re_path(r'^api/course/(?P<course_code>.*)', CourseApiView.as_view())
 ]
