3531977746b5135c04e6daca4a567eb6875624ef
Charlon Palacay <charlon@gmail.com>
2019-03-01 10:01:42 -0800

    Create and stub out course search and detail vue components.

---
+++ prereq_map/static/prereq_map/js/components/course-search.vue
@@ -0,0 +1,32 @@
+<template>
+  <div style="margin-bottom:25px;" class="course-search">
+    <h3>{{ blah }}</h3>
+    <form>
+      <div id="main-search" class="search-width">
+        <div class="d-table-cell w-100">
+          <input class="form-control form-control-lg form-styling" aria-describedby="fieldHelp" placeholder="e.g. BIOL 300">
+        </div>
+        <div class="d-table-cell align-middle">
+          <button class="btn btn-primary button-styling"><i class="py-1 fas fa-search"></i></button>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        blah: 'This is extra!'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.course-search {
+  border: solid 1px lime;
+  padding: 10px;
+}
+</style>
--- prereq_map/static/prereq_map/js/course.js
+++ prereq_map/static/prereq_map/js/course.js
@@ -1,6 +1,8 @@
 import Vue from 'vue/dist/vue.js';
 
 import ButtonCounter from "./components/button-counter.vue";
+import CourseSearch from "./components/course-search.vue";
+import CourseDetail from "./components/course-detail.vue";
 
 console.log("I am Vue!")
 
@@ -8,6 +10,8 @@ new Vue({
   delimiters: ['[[', ']]'],
   el: '#vue_course',
   components: {
-    'button-counter' : ButtonCounter
+    'button-counter' : ButtonCounter,
+    'course-search' : CourseSearch,
+    'course-detail' : CourseDetail
   }
 })
