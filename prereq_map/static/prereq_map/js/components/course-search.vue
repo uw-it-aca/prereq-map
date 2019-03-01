32b4cccdb52bc8dfa56d59db3800aea677e50675
Charlon Palacay <charlon@gmail.com>
2019-03-01 10:55:51 -0800

    Wire course search input to query param.

--- prereq_map/static/prereq_map/js/components/course-search.vue
+++ prereq_map/static/prereq_map/js/components/course-search.vue
@@ -1,26 +1,40 @@
 <template>
   <div style="margin-bottom:25px;" class="course-search">
-    <h3>{{ blah }}</h3>
-    <form>
+
+    <form id="signup-form" @submit.prevent="processForm">
       <div id="main-search" class="search-width">
         <div class="d-table-cell w-100">
-          <input class="form-control form-control-lg form-styling" aria-describedby="fieldHelp" placeholder="e.g. BIOL 300">
+          <input class="form-control form-control-lg form-styling" placeholder="e.g. BIOL 300" v-model="course">
         </div>
         <div class="d-table-cell align-middle">
           <button class="btn btn-primary button-styling"><i class="py-1 fas fa-search"></i></button>
         </div>
       </div>
     </form>
+
   </div>
 </template>
 
 <script>
+
   export default {
     data () {
       return {
-        blah: 'This is extra!'
+        course: '',
+      }
+    },
+
+    methods: {
+      processForm: function() {
+
+        console.log({ course: this.course });
+
+        // send course
+        window.location.href = "/course-search/?course=" + this.course;
+
       }
     }
+
   }
 </script>
 
