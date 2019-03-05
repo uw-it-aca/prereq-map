025d74f1d570524ea1b6e328b116cb85fdd5a126
Charlon Palacay <charlon@gmail.com>
2019-03-05 14:00:04 -0800

    4 space tabs.

--- prereq_map/static/prereq_map/js/components/course-search.vue
+++ prereq_map/static/prereq_map/js/components/course-search.vue
@@ -1,46 +1,41 @@
 <template>
-  <div style="margin-bottom:25px;" class="course-search">
-
+<div style="margin-bottom:25px;" class="course-search">
     <form id="signup-form" @submit.prevent="processForm">
-      <div id="main-search" class="search-width">
-        <div class="d-table-cell w-100">
-          <input class="form-control form-control-lg form-styling" placeholder="e.g. BIOL 300" v-model="course">
+        <div id="main-search" class="search-width">
+            <div class="d-table-cell w-100">
+                <input class="form-control form-control-lg form-styling" placeholder="e.g. BIOL 300" v-model="course">
+            </div>
+            <div class="d-table-cell align-middle">
+                <button class="btn btn-primary button-styling"><i class="py-1 fas fa-search"></i></button>
+            </div>
         </div>
-        <div class="d-table-cell align-middle">
-          <button class="btn btn-primary button-styling"><i class="py-1 fas fa-search"></i></button>
-        </div>
-      </div>
     </form>
-
-  </div>
+</div>
 </template>
 
 <script>
-
-  export default {
-    data () {
-      return {
-        course: '',
-      }
+export default {
+    data() {
+        return {
+            course: '',
+        }
     },
 
     methods: {
-      processForm: function() {
+        processForm: function() {
 
-        console.log({ course: this.course });
+            // send course
+            window.location.href = "/course-search/?course=" + this.course;
 
-        // send course
-        window.location.href = "/course-search/?course=" + this.course;
-
-      }
+        }
     }
 
-  }
+}
 </script>
 
 <style scoped>
 .course-search {
-  border: solid 1px lime;
-  padding: 10px;
+    border: solid 1px lime;
+    padding: 10px;
 }
 </style>
