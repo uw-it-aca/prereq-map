aeea778304397c804313646d185a4d9cfe2ee46f
Charlon Palacay <charlon@gmail.com>
2019-03-06 12:21:41 -0800

    Add fixture command to docker compose build process.

--- webpack.config.js
+++ webpack.config.js
@@ -15,12 +15,6 @@ module.exports = {
   optimization: {
 		splitChunks: {
 			cacheGroups: {
-				commons: {
-					chunks: "initial",
-					minChunks: 2,
-					maxInitialRequests: 5, // The default limit is too small to showcase the effect
-					minSize: 0 // This is example is too small to create commons chunks
-				},
 				vendor: {
 					test: /node_modules/,
 					chunks: "initial",
