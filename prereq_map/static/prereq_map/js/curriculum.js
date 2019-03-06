f38c5d829eff6a53d6e2a7062008997e9d7dba69
Charlon Palacay <charlon@gmail.com>
2019-03-06 12:35:09 -0800

    Conditional rendering of curric map based on query params.

--- prereq_map/static/prereq_map/js/curriculum.js
+++ prereq_map/static/prereq_map/js/curriculum.js
@@ -1,6 +1,5 @@
 import Vue from 'vue/dist/vue.js';
 
-import ButtonCounter from "./components/button-counter.vue";
 import CourseInfoBox from "./components/course-infobox.vue";
 import Typeahead from "./components/typeahead.vue";
 
@@ -10,7 +9,6 @@ new Vue({
   delimiters: ['[[', ']]'],
   el: '#vue_curriculum',
   components: {
-    'button-counter' : ButtonCounter,
     'course-infobox' : CourseInfoBox,
     'typeahead' : Typeahead
   }
