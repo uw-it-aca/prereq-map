f4653651c078f6129491e40b639165637bed1458
Charlon Palacay <charlon@gmail.com>
2019-03-22 08:09:34 -0700

    Working jest image snapshot example.

---
+++ prereq_map/static/prereq_map/js/__tests__/example.spec.js
@@ -0,0 +1,43 @@
+import { mount } from '@vue/test-utils'
+import Onbaording from '../components/onboarding.vue'
+
+const { toMatchImageSnapshot } = require('jest-image-snapshot');
+
+//import { toMatchImageSnapshot } from 'jest-image-snapshot'
+expect.extend({ toMatchImageSnapshot });
+
+const puppeteer = require('puppeteer');
+
+describe('Example Component', () => {
+
+    let browser;
+
+    beforeAll(async () => {
+        browser = await puppeteer.launch();
+    });
+
+    test('is a Vue instance', () => {
+        const wrapper = mount(Onbaording)
+        expect(wrapper.isVueInstance()).toBeTruthy()
+    });
+
+    test('renders correctly', () => {
+        const wrapper = mount(Onbaording)
+        expect(wrapper.element).toMatchSnapshot()
+    });
+
+    test('visual regression', async () => {
+
+        const page = await browser.newPage();
+        await page.goto('http://localhost:8123/course-search/');
+        const screenshot = await page.screenshot();
+        expect(screenshot).toMatchImageSnapshot();
+
+
+    });
+
+    afterAll(async () => {
+        await browser.close();
+    });
+
+})
