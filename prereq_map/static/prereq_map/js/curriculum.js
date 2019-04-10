5c109cd281eb307501a326ee2f303d6074d85ceb
Charlon Palacay <charlon@gmail.com>
2019-04-10 15:56:12 -0700

    Refactor curric js and graph.

--- prereq_map/static/prereq_map/js/curriculum.js
+++ prereq_map/static/prereq_map/js/curriculum.js
@@ -22,59 +22,8 @@ new Vue({
     router,
     components: {
         'onboarding' : Onboarding,
-        'course-infobox': CourseInfoBox,
         'typeahead': Typeahead,
-        'curric-graph': Graph
-    },
-    data() {
-        return {
-            curric_param: '',
-            curric_data: [],
-            course_list: [],
-            curric_emtpy: undefined
-        }
-    },
-    methods: {
-
-        getCurric: function() {
-            return axios
-                .get('/api/curric/' + encodeURI(this.curric_param))
-                .then(response => {
-                    this.curric_data = response.data
-                    //filter the data to just course_number
-                    this.course_list = this.curric_data.x.nodes.course_number
-
-                    // check to see if course_list is empty
-                    if (Object.keys(this.course_list).length !== 0) {
-                        this.curric_emtpy = false
-                    } else {
-                        this.curric_emtpy = true
-                    }
-                })
-                .catch(error => {
-                    console.log(error)
-                })
-        }
-    },
-    mounted() {
-
-        this.curric_param = this.$route.query.curric
-
-        if (this.curric_param !== undefined) {
-            this.getCurric()
-        }
-
-    },
-    watch: {
-
-        '$route.query.curric': function () {
-            //console.log("curric changed")
-            this.curric_param = this.$route.query.curric
-
-            if (this.curric_param !== undefined) {
-                this.getCurric()
-            }
-        }
-
+        'curric-graph': Graph,
+        'course-infobox': CourseInfoBox
     },
 });
