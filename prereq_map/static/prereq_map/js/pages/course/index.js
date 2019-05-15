88d289595a72d78b4a41fe70a84af123b2544633
Charlon Palacay <charlon@gmail.com>
2019-05-15 15:56:54 -0700

    Course page GA tracking enabled.

--- prereq_map/static/prereq_map/js/pages/course/index.js
+++ prereq_map/static/prereq_map/js/pages/course/index.js
@@ -1,6 +1,9 @@
-import Vue from 'vue/dist/vue.js';
+import Vue from 'vue/dist/vue.js'
 import VueRouter from 'vue-router/dist/vue-router.js'
+import VueAnalytics from 'vue-analytics'
 import App from './app.vue'
+
+const gaCode = $("body").data("google-analytics");
 
 Vue.use(VueRouter)
 
@@ -10,6 +13,14 @@ var router = new VueRouter({
         path: '/course-search/'
     }, ]
 });
+
+Vue.use(VueAnalytics, {
+  id: gaCode,
+  router,
+  debug: {
+    enabled: true
+  }
+})
 
 export const dataBus = new Vue();
 
