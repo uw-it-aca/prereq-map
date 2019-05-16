3261d8775e32b6ada5656d1c027a7a5b3532c5b0
Stephen De Vight <devights@gmail.com>
2019-05-15 17:13:30 -0700

    build tweak

--- prereq_map/management/commands/restart_build_graphs.py
+++ prereq_map/management/commands/restart_build_graphs.py
@@ -1,7 +1,9 @@
 from django.core.management.base import BaseCommand
-from prereq_map.utils.build_graphs import rebuild_graphs
+from prereq_map.utils.build_graphs import build_course_graphs, \
+    build_curric_graphs
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        rebuild_graphs()
+        build_curric_graphs()
+        build_course_graphs(900000)
