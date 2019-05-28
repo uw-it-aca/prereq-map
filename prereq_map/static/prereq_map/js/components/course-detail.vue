b88d7effcabf07478c6f6487d191498638803e7c
Charlon Palacay <charlon@gmail.com>
2019-05-28 11:10:17 -0700

    Updates course details per MyPlan design team suggestion.

--- prereq_map/static/prereq_map/js/components/course-detail.vue
+++ prereq_map/static/prereq_map/js/components/course-detail.vue
@@ -26,7 +26,7 @@
             </div>
 
             <strong>Additional information:</strong>
-            <p><a v-bind:href="'https://myplan.uw.edu/course/#/courses/' + courseParam" v-on:click="$ga.event('outbound', 'click', 'https://myplan.uw.edu/course/#/courses/' + courseParam)" target="_blank">View course details on MyPlan</a></p>
+            <p><a v-bind:href="'https://myplan.uw.edu/course/#/courses/' + courseParam" v-on:click="$ga.event('outbound', 'click', 'https://myplan.uw.edu/course/#/courses/' + courseParam)" target="_blank">View {{courseParam}} course details and schedule on MyPlan</a></p>
 
         </div>
     </div>
