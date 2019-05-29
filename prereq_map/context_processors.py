92a4b35bfe5b457b0f11f17451bbfc343fac6e11
Stephen De Vight <devights@gmail.com>
2019-05-29 13:01:53 -0700

    changing default key to be compatible with vue-analytics

--- prereq_map/context_processors.py
+++ prereq_map/context_processors.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 def google_analytics(request):
 
-    ga_key = getattr(settings, 'GOOGLE_ANALYTICS_KEY', False)
+    ga_key = getattr(settings, 'GOOGLE_ANALYTICS_KEY', " ")
     return {
         'google_analytics': ga_key
     }
