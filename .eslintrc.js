9d540586476593164ca6474b787c3553d9bc131c
Charlon Palacay <charlon@gmail.com>
2019-05-29 20:44:12 -0700

    Adds stylelint and stylelint sass plugin.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,17 @@
 module.exports = {
-    extends: [
-      // add more generic rulesets here, such as:
-      // 'eslint:recommended',
-      'plugin:vue/recommended'
-    ],
-    rules: {
-      // override/add rules settings here, such as:
-      // 'vue/no-unused-vars': 'error'
-    }
-  }
+  extends: [
+    // add more generic rulesets here, such as:
+    // 'eslint:recommended',
+    'plugin:vue/recommended'
+  ],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  },
+  env: {
+    "browser": true,
+    "commonjs": true,
+    "es6": true,
+    "jquery": true
+  },
+};
