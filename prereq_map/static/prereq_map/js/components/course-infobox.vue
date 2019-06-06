303c2e9304f3c89d6fc52f28c9441d70b87973ee
Charlon Palacay <charlon@gmail.com>
2019-06-06 11:45:17 -0700

    Display entire course infobox after loading completes.

--- prereq_map/static/prereq_map/js/components/course-infobox.vue
+++ prereq_map/static/prereq_map/js/components/course-infobox.vue
@@ -1,169 +1,184 @@
 <template>
-<div>
-    <div class="card mt-4" v-if="course_code">
+  <div>
+    <div v-if="course_code" class="card mt-4">
+      <div v-if="loading">
         <div class="card-header bg-white">
-            <h5 class="m-0">{{ course_code }}</h5>
-            <!--<a href="#" class="prereq-infobox-close" v-on:click.stop.prevent="close"><i class="fas fa-times"></i></a>-->
-            <p class="card-title" v-if="course_description" v-shave="{ height: 150 }">{{ course_description }}</p>
+          Loading.....
+        </div>
+      </div>
+      <div v-else>
+        <div class="card-header bg-white">
+          <h5 class="m-0">
+            {{ course_code }}
+          </h5>
 
-            <a v-bind:href="'/course-search/?course=' + course_code" class="btn btn-primary btn-sm prereq-infobox-button">More details</a>
+          <p v-if="course_description" class="card-title">
+            {{ course_description }}
+          </p>
 
+          <a
+            :href="'/course-search/?course=' + course_code"
+            class="btn btn-primary btn-sm prereq-infobox-button"
+            >More details</a
+          >
         </div>
         <div class="card-body bg-light">
-
-            <div v-if="loading">Loading.....</div>
-            <div v-else>
-                <h5 class="card-title h6">Is a prerequisite for:</h5>
-                <ul class="prereq-list">
-                    <li v-if="postreqs.length === 0">none</li>
-                    <li v-for="postreq in postreqs">
-                        <a v-bind:href="'/course-search/?course=' + postreq">{{postreq}}</a>
-                    </li>
-                </ul>
-
-            </div>
+          <h5 class="card-title h6">
+            Is a prerequisite for:
+          </h5>
+          <ul class="prereq-list">
+            <li v-if="postreqs.length === 0">
+              none
+            </li>
+            <li v-for="postreq in postreqs" :key="postreq">
+              <a :href="'/course-search/?course=' + postreq">{{ postreq }}</a>
+            </li>
+          </ul>
         </div>
+      </div>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
 //import { dataBus } from "../pages/course/";
-import Vue from 'vue/dist/vue.esm.js';
-import VueShave from 'vue-shave';
-Vue.use( VueShave );
+import Vue from "vue/dist/vue.esm.js";
 
-const axios = require('axios');
+const axios = require("axios");
 export default {
-    data() {
-        return {
-            loading: true,
-            course_code: '',
-            last_selected: '',
-            prereqs: [],
-            postreqs: [],
-            course_data: undefined,
-            course_description: ''
+  data() {
+    return {
+      loading: true,
+      course_code: "",
+      last_selected: "",
+      prereqs: [],
+      postreqs: [],
+      course_data: undefined,
+      course_description: ""
+    };
+  },
+  mounted() {
+    this.course_code = this.$route.query.course;
+    if (this.course_code !== undefined) {
+      this.show(this.course_code);
+
+      // update page title
+      document.title = this.course_code + " - Curriculum Search - Prereq Map";
+    }
+
+    // global click handler for show node event
+    $(document).on("showCourseInfo", (event, course_code) => {
+      this.show(course_code);
+
+      // https://developers.google.com/analytics/devguides/collection/analyticsjs/events
+      // google event category, action, label, value
+      this.$ga.event("curric map", "clicked course node", course_code);
+    });
+
+    // global click handler for close node event
+
+    $(document).on("closeCourseInfo", event => {
+      this.close();
+    });
+  },
+  methods: {
+    load_course: function(course_code) {
+      axios.get("/api/course/" + encodeURI(course_code)).then(response => {
+        this.course_data = response;
+        this.loading = false;
+      });
+    },
+    get_prereqs: function(course, from_list) {
+      var keys = Object.keys(from_list);
+      var from = [];
+      keys.forEach(function(key) {
+        var value = from_list[key];
+        if (course !== value && !from.includes(value)) {
+          from.push(value);
         }
+      });
+      return from.sort();
     },
-    mounted() {
-
-        this.course_code = this.$route.query.course;
-        if(this.course_code !== undefined){
-            this.show(this.course_code);
-
-            // update page title
-            document.title = this.course_code + ' - Curriculum Search - Prereq Map';
-
+    get_postreqs: function(course, to_list) {
+      var keys = Object.keys(to_list);
+      var to = [];
+      keys.forEach(function(key) {
+        var value = to_list[key];
+        if (course !== value && !to.includes(value)) {
+          to.push(value);
         }
-
-        // global click handler for show node event
-        $(document).on('showCourseInfo', (event, course_code) => {
-            this.show(course_code);
-
-            // https://developers.google.com/analytics/devguides/collection/analyticsjs/events
-            // google event category, action, label, value
-            this.$ga.event('curric map', 'clicked course node', course_code)
-        });
-
-        // global click handler for close node event
-
-        $(document).on('closeCourseInfo', (event) => {
-            this.close();
-        });
-
-
+      });
+      return to.sort();
     },
-    methods: {
-        load_course: function(course_code) {
-            axios.get('/api/course/' + encodeURI(course_code))
-                .then(response => {
-                    this.course_data = response
-                    this.loading = false
-                })
-        },
-        get_prereqs: function (course, from_list) {
-            var keys = Object.keys(from_list);
-            var from = [];
-            keys.forEach(function(key){
-                var value = from_list[key];
-                if(course !== value && !from.includes(value)){
-                    from.push(value)
-                }
-            });
-            return from.sort();
-        },
-        get_postreqs: function (course, to_list){
-            var keys = Object.keys(to_list);
-            var to = [];
-            keys.forEach(function(key){
-                var value = to_list[key];
-                if(course !== value && !to.includes(value)){
-                    to.push(value)
-                }
-            });
-            return to.sort();
-        },
-        show: function (code) {
+    show: function(code) {
+      this.loading = true;
+      this.course_code = code;
+      this.$router.push({
+        query: Object.assign({}, this.$route.query, {
+          course: this.course_code
+        })
+      });
 
-            this.loading = true
-            this.course_code = code;
-            this.$router.push({ query: Object.assign({}, this.$route.query, { course: this.course_code }) });
-
-            if (this.course_code !== undefined) {
-                this.load_course(code);
-            }
-        },
-
-        close: function() {
-            // set course param to undefined in order to clear it out from query
-            this.$router.replace({ query: Object.assign({}, this.$route.query, { course: undefined }) });
-        },
-
+      if (this.course_code !== undefined) {
+        this.load_course(code);
+      }
     },
 
-    watch: {
+    close: function() {
+      // set course param to undefined in order to clear it out from query
+      this.$router.replace({
+        query: Object.assign({}, this.$route.query, { course: undefined })
+      });
+    }
+  },
 
-        '$route.query.course': function () {
-            this.course_code = this.$route.query.course;
-            this.show(this.course_code)
+  watch: {
+    "$route.query.course": function() {
+      this.course_code = this.$route.query.course;
+      this.show(this.course_code);
 
-            if (this.course_code !== undefined) {
-                // update page title
-                document.title = this.course_code + ' - Curriculum Search - Prereq Map';
-            } else {
-                // update page title
-                document.title = this.$route.query.curric + ' - Curriculum Search - Prereq Map';
-            }
-
-        },
-        course_data: function () {
-            this.prereqs = this.get_prereqs(this.course_code, this.course_data.data.x.edges.from);
-            this.postreqs = this.get_postreqs(this.course_code, this.course_data.data.x.edges.to);
-            this.course_description = this.course_data.data.course_description;
-        }
-
+      if (this.course_code !== undefined) {
+        // update page title
+        document.title = this.course_code + " - Curriculum Search - Prereq Map";
+      } else {
+        // update page title
+        document.title =
+          this.$route.query.curric + " - Curriculum Search - Prereq Map";
+      }
     },
-}
+    course_data: function() {
+      this.prereqs = this.get_prereqs(
+        this.course_code,
+        this.course_data.data.x.edges.from
+      );
+      this.postreqs = this.get_postreqs(
+        this.course_code,
+        this.course_data.data.x.edges.to
+      );
+      this.course_description = this.course_data.data.course_description;
+    }
+  }
+};
 </script>
 
 <style lang="scss">
-
 .prereq-infobox-close {
-    position: absolute;
-    top: 0.375rem;
-    right: 0.375rem;
-    z-index: 10;
-    display: block;
-    padding: 0.25rem 0.5rem;
-    color: #000;
+  position: absolute;
+  top: 0.375rem;
+  right: 0.375rem;
+  z-index: 10;
+  display: block;
+  padding: 0.25rem 0.5rem;
+  color: #000;
 
-    &:hover { color: #666; }
+  &:hover {
+    color: #666;
+  }
 }
 
 .prereq-infobox-button {
-    background-color: #4d307f;
-    color: #FFF;
-    border: none;
+  background-color: #4d307f;
+  color: #fff;
+  border: none;
 }
 </style>
