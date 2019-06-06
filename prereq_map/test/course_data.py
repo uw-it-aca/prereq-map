f3666a62a25653c77cb0cd56d13f09526ff2b758
Stephen De Vight <devights@gmail.com>
2019-06-05 17:10:36 -0700

    fix 2

--- prereq_map/test/course_data.py
+++ prereq_map/test/course_data.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, Client
-from prereq_map.utils.course_data import get_section_details,\
-    split_course, get_course_label
+from prereq_map.utils.course_data import split_course, get_course_label, \
+    get_course_details
 from uw_sws.term import get_term_by_year_and_quarter
 
 
@@ -14,8 +14,15 @@ class TestCourse(TestCase):
     def test_course_label(self):
         term = get_term_by_year_and_quarter(2013, 'spring')
         self.assertEqual(get_course_label(term, "CSE 142"),
-                         "2013,spring,CSE,142/A")
+                         "2013,spring,CSE,142")
 
-    def test_get_section(self):
-        section = get_section_details("CSE 142")
+    def test_get_course(self):
+        section = get_course_details("CSE 142")
         self.assertIsNotNone(section)
+        desc = "Basic programming-in-the-small abilities and concepts" \
+               " including procedural programming (methods, parameters, " \
+               "return, values), basic control structures (sequence, " \
+               "if/else, for loop, while loop), file processing, arrays," \
+               " and an introduction to defining objects. Intended for " \
+               "students without prior programming experience. Offered: AWSpS."
+        self.assertEqual(section.course_description, desc)
