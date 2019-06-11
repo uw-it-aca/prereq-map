65cd1505462dae3a5b777eb7a728407060f915e3
Charlon Palacay <charlon@gmail.com>
2019-06-11 09:14:39 -0700

    Clear previous curric data if new search returns errror.

--- prereq_map/static/prereq_map/js/components/curric-graph.vue
+++ prereq_map/static/prereq_map/js/components/curric-graph.vue
@@ -5,10 +5,14 @@
         >Use the scroll function on your mouse or touchpad to zoom in and
         out</small
       >
+
       <div id="graph_container"></div>
-      <small>The map only displays prerequisite relationships within the selected
-          curriculum. View course details to see prerequisites from other
-          curricula.</small>
+
+      <small
+        >The map only displays prerequisite relationships within the selected
+        curriculum. View course details to see prerequisites from other
+        curricula.</small
+      >
     </div>
     <div v-if="graph_error === true">
       <p>
@@ -28,53 +32,36 @@
 </template>
 
 <script>
-const axios = require("axios");
-export default {
-  data() {
-    return {
-      curric_param: undefined,
-      course_param: undefined,
-      curric_data: [],
-      course_list: [],
-      graph_error: undefined
-    };
-  },
-  methods: {
-    getCurric: function() {
-      return axios
-        .get("/api/curric/" + encodeURI(this.curric_param))
-        .then(response => {
-          this.curric_data = response.data;
+  const axios = require("axios");
+  export default {
+    data() {
+      return {
+        curric_param: undefined,
+        course_param: undefined,
+        curric_data: [],
+        course_list: [],
+        graph_error: undefined
+      };
+    },
+    methods: {
+      getCurric: function() {
+        return axios
+          .get("/api/curric/" + encodeURI(this.curric_param))
+          .then(response => {
+            this.curric_data = response.data;
 
-          this.graph_error = false;
-          this.loading = false;
-        })
-        .catch(error => {
-          this.graph_error = true;
-        });
-    }
-  },
-
-  mounted() {
-    this.curric_param = this.$route.query.curric;
-    this.course_param = this.$route.query.course;
-
-    if (this.curric_param !== undefined) {
-      this.getCurric();
-
-      // update page title
-      document.title = this.curric_param + " - Curriculum Search - Prereq Map";
-    }
-  },
-
-  watch: {
-    curric_data: function() {
-      show_graph(this.curric_data, this.course_param);
+            this.graph_error = false;
+            this.loading = false;
+          })
+          .catch(error => {
+            // show the graph error and clear previous curric data
+            this.graph_error = true;
+            this.curric_data = [];
+          });
+      }
     },
 
-    "$route.query.curric": function() {
-      // react to route changes...
-
+    mounted() {
       this.curric_param = this.$route.query.curric;
       this.course_param = this.$route.query.course;
 
@@ -82,10 +69,29 @@ export default {
         this.getCurric();
 
         // update page title
-        document.title =
-          this.curric_param + " - Curriculum Search - Prereq Map";
+        document.title = this.curric_param + " - Curriculum Search - Prereq Map";
+      }
+    },
+
+    watch: {
+      curric_data: function() {
+        show_graph(this.curric_data, this.course_param);
+      },
+
+      "$route.query.curric": function() {
+        // react to route changes...
+
+        this.curric_param = this.$route.query.curric;
+        this.course_param = this.$route.query.course;
+
+        if (this.curric_param !== undefined) {
+          this.getCurric();
+
+          // update page title
+          document.title =
+            this.curric_param + " - Curriculum Search - Prereq Map";
+        }
       }
     }
-  }
-};
+  };
 </script>
