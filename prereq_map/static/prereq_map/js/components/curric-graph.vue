8c5f1087d9740dbe03d6c135a4a05dd10d417fa6
Charlon Palacay <charlon@gmail.com>
2019-06-13 13:40:34 -0700

    Fixes all linting errors for vue comps.

--- prereq_map/static/prereq_map/js/components/curric-graph.vue
+++ prereq_map/static/prereq_map/js/components/curric-graph.vue
@@ -1,16 +1,16 @@
 <template>
   <div v-if="curric_param !== undefined">
     <div>
-      <small v-if="graph_error === false" class="text-secondary"
-        >Use the scroll function on your mouse or touchpad to zoom in and
-        out</small
-      >
-      <div id="graph_container"></div>
-      <small v-if="graph_error === false"
-        >The map only displays prerequisite relationships within the selected
+      <small v-if="graph_error === false" class="text-secondary">
+        Use the scroll function on your mouse or touchpad to zoom in and
+        out
+      </small>
+      <div id="graph_container" />
+      <small v-if="graph_error === false">
+        The map only displays prerequisite relationships within the selected
         curriculum. View course details to see prerequisites from other
-        curricula.</small
-      >
+        curricula.
+      </small>
     </div>
     <div v-if="graph_error === true">
       <p>
@@ -41,39 +41,9 @@
         graph_error: undefined
       };
     },
-    methods: {
-      getCurric: function() {
-        return axios
-          .get("/api/curric/" + encodeURI(this.curric_param))
-          .then(response => {
-            this.curric_data = response.data;
-
-            this.graph_error = false;
-            this.loading = false;
-          })
-          .catch(error => {
-            // show the graph error and clear previous curric data
-            this.graph_error = true;
-            this.curric_data = [];
-          });
-      }
-    },
-
-    mounted() {
-      this.curric_param = this.$route.query.curric;
-      this.course_param = this.$route.query.course;
-
-      if (this.curric_param !== undefined) {
-        this.getCurric();
-
-        // update page title
-        document.title = this.curric_param + " - Curriculum Search - Prereq Map";
-      }
-    },
-
     watch: {
       curric_data: function() {
-        show_graph(this.curric_data, this.course_param);
+        window.show_graph(this.curric_data, this.course_param);
       },
 
       "$route.query.curric": function() {
@@ -89,6 +59,34 @@
           document.title =
             this.curric_param + " - Curriculum Search - Prereq Map";
         }
+      }
+    },
+    mounted() {
+      this.curric_param = this.$route.query.curric;
+      this.course_param = this.$route.query.course;
+
+      if (this.curric_param !== undefined) {
+        this.getCurric();
+
+        // update page title
+        document.title = this.curric_param + " - Curriculum Search - Prereq Map";
+      }
+    },
+    methods: {
+      getCurric: function() {
+        return axios
+          .get("/api/curric/" + encodeURI(this.curric_param))
+          .then(response => {
+            this.curric_data = response.data;
+
+            this.graph_error = false;
+            this.loading = false;
+          })
+          .catch(() => {
+            // show the graph error and clear previous curric data
+            this.graph_error = true;
+            this.curric_data = [];
+          });
       }
     }
   };
--- prereq_map/static/prereq_map/js/components/typeahead.vue
+++ prereq_map/static/prereq_map/js/components/typeahead.vue
@@ -1,86 +1,71 @@
 <template>
-<div class="row curric-search">
+  <div class="row curric-search">
     <div class="col-md-9 offset-md-1">
-        <vue-bootstrap-typeahead
-            class="mb-3"
-            v-model="query"
-            :data="curric_list"
-            @hit="selected_curric = $event"
-            placeholder="E.G. ANTH"
-        />
+      <vue-bootstrap-typeahead
+        v-model="query"
+        class="mb-3"
+        :data="curric_list"
+        placeholder="E.G. ANTH"
+        @hit="selected_curric = $event"
+      />
     </div>
-</div>
+  </div>
 </template>
 
 <script>
-    import {_} from 'vue-underscore';
-    const axios = require('axios');
-    import VueBootstrapTypeahead from 'vue-bootstrap-typeahead';
+  const axios = require("axios");
+  import VueBootstrapTypeahead from "vue-bootstrap-typeahead";
 
-    export default {
+  export default {
+    components: {
+      VueBootstrapTypeahead
+    },
+    filters: {},
+    data() {
+      return {
+        query: "",
+        query2: "",
+        selected_curric: null,
+        curric_list: [],
+        curric_objs: null
+      };
+    },
 
-        components: {
-            VueBootstrapTypeahead
-        },
+    watch: {
+      selected_curric(curric_query) {
+        var curric_code = this.curric_objs[curric_query];
+        //location.href = "?curric=" + curric_code;
+        this.$router.push("/curriculum-search/?curric=" + curric_code);
+      },
+    },
+    mounted() {
+      axios.get("/api/curric_typeahead").then(res => {
+        this.curric_objs = res.data;
+        var curric_list = [];
 
-        data() {
-            return {
-                query: '',
-                query2: '',
-                selected_curric: null,
-                curric_list: [],
-                curric_objs: null
-            }
-        },
-        mounted() {
-            var self = this;
-            axios.get('/api/curric_typeahead')
-                .then((res) => {
-                    this.curric_objs = res.data;
-                    var curric_list = [];
-
-                    $(res.data).each(function(idx, value){
-                       curric_list.push(...Object.keys(value));
-                    });
-                    this.curric_list = curric_list;
-                });
-        },
-
-        watch: {
-            selected_curric(curric_query){
-                var curric_code = this.curric_objs[curric_query];
-                //location.href = "?curric=" + curric_code;
-                this.$router.push('/curriculum-search/?curric=' + curric_code)
-
-            },
-
-            '$route'(to, from) {
-                // react to route changes...
-            }
-
-        },
-
-        filters: {
-        },
-
+        $(res.data).each(function(idx, value) {
+          curric_list.push(...Object.keys(value));
+        });
+        this.curric_list = curric_list;
+      });
     }
+  };
 </script>
 
 <style lang="scss">
-// char's note: scoped css is "broken" for this component because
-//  vue-bootstrap-typeahead is already a scoped component by default... so it
-// cannot be scoped again. just be aware of this in the future!
+  // char's note: scoped css is "broken" for this component because
+  //  vue-bootstrap-typeahead is already a scoped component by default... so it
+  // cannot be scoped again. just be aware of this in the future!
 
-.curric-search {
-
+  .curric-search {
     input {
-        height: 3rem;
-        font-size: 1.25rem;
-        color: #000;
+      color: #000;
+      font-size: 1.25rem;
+      height: 3rem;
     }
 
     .vbt-autcomplete-list {
-        box-shadow: none !important;
+      box-shadow: none !important;
     }
-}
+  }
 </style>
