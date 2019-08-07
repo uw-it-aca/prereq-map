a5ad2a7a0fba2dd544ddcf0b1fbfe3803016b986
Charlon Palacay <charlon@gmail.com>
2019-08-07 11:12:12 -0700

    Moved curric template into new curric vue page.

--- prereq_map/static/prereq_map/js/pages/index/curriculum.vue
+++ prereq_map/static/prereq_map/js/pages/index/curriculum.vue
@@ -1,10 +1,46 @@
 // curriculum.vue
 
 <template>
-  <div>
-    Curriculum
+  <div id="curriculum" class="container py-1 mt-2" role="main">
+    <onboarding />
+
+    <h1 class="pt-4 pb-2">
+      Curriculum Search
+    </h1>
+    <p class="instruction-text pb-4">
+      View the prerequisite map of courses in a curriculum
+    </p>
+
+    <typeahead />
+
+    <div class="row mt-5 mb-5">
+      <div class="col-md-9 mb-3">
+        <curric-graph />
+      </div>
+      <div class="col-sm-12 col-md-3">
+        <course-infobox ref="infobox" />
+      </div>
+    </div>
   </div>
 </template>
+
 <script>
-  export default {};
+  import Onboarding from "../../components/onboarding.vue";
+  import CourseInfoBox from "../../components/course-infobox.vue";
+  import Typeahead from "../../components/typeahead.vue";
+  import Graph from "../../components/curric-graph.vue";
+
+  export default {
+    name: "Curriculum",
+    components: {
+      onboarding: Onboarding,
+      typeahead: Typeahead,
+      "curric-graph": Graph,
+      "course-infobox": CourseInfoBox
+    },
+    mounted() {}
+  };
 </script>
+
+<style lang="scss">
+</style>
