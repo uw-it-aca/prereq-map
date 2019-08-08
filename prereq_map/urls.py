173981f2813444376381b377ec9cfabee58c51a6
Charlon Palacay <charlon@gmail.com>
2019-08-08 15:27:00 -0700

    Use re path for mapping base url to serve vue.

--- prereq_map/urls.py
+++ prereq_map/urls.py
@@ -1,5 +1,4 @@
 from django.urls import include, path, re_path
-from django.conf.urls import url
 from django.views.generic.base import RedirectView
 from prereq_map.views.pages import PageView, CurriculumSearch, CourseSearch
 from prereq_map.views.api import CurricApiView, CourseApiView, \
@@ -7,11 +6,10 @@ from prereq_map.views.api import CurricApiView, CourseApiView, \
 
 urlpatterns = [
     # path('', RedirectView.as_view(url='/curriculum-search/')),
-    path('curriculum-search/', CurriculumSearch.as_view()),
-    path('course-search/', CourseSearch.as_view()),
+    # path('curriculum-search/', CurriculumSearch.as_view()),
+    # path('course-search/', CourseSearch.as_view()),
     re_path(r'^api/curric/(?P<curric_code>.*)', CurricApiView.as_view()),
     re_path(r'^api/course/(?P<course_code>.*)', CourseApiView.as_view()),
     re_path(r'^api/curric_typeahead', CurricTypeaheadApiView.as_view()),
-    # path('', PageView.as_view(), name="index")
-    url(r'^.*$', PageView.as_view(), name='index'),
+    re_path(r'^.*$', PageView.as_view(), name='index')
 ]
