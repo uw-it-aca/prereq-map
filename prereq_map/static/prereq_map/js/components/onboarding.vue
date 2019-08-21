782f92f92f8d12d81b36af335d4cab4cf6eb411f
Charlon Palacay <charlon@gmail.com>
2019-08-21 13:25:47 -0700

    Convert onboarding to use native vue bootstrap components.

--- prereq_map/static/prereq_map/js/components/onboarding.vue
+++ prereq_map/static/prereq_map/js/components/onboarding.vue
@@ -1,5 +1,145 @@
 <template>
+  <div>
+    <b-modal id="bv-modal-example" size="lg" centered hide-header hide-footer>
+      <div class="d-block">
+        <b-carousel
+          id="carousel-1"
+          :interval="0"
+          controls
+          indicators
+          img-width="1024"
+          img-height="880"
+        >
+          <!-- Slides with custom text -->
+          <b-carousel-slide img-src="/static/prereq_map/img/modal-bg.jpg" img-alt="fsdaasdf">
+            <div class="text-left text-dark">
+              <p class="mb-4 h2">
+                Discover courses and plan your schedule more effectively
+              </p>
+              <p>
+                It can be tough figuring out which courses to take. How do
+                you find courses similar to ones you’ve really enjoyed? Of
+                the courses you’ve already taken, which ones provide a
+                foundation for more advanced coursework? What sequence of
+                courses is best? The <strong>Prereq Map</strong> helps you
+                discover interesting courses and enables you to be strategic
+                about planning your course schedule. (What’s a prereq?
+                Prereq is short for prerequisites: the courses that act as a
+                foundation for other courses.)
+              </p>
+              <p>Remember to talk to your adviser when course planning.</p>
+            </div>
+          </b-carousel-slide>
+
+          <!-- Slides with custom text -->
+          <b-carousel-slide img-src="/static/prereq_map/img/modal-bg.jpg" img-alt="fsdaasdf">
+            <div class="text-left text-dark">
+              <p class="mt-4 mb-4 h3">
+                See the prerequisite map for a curriculum
+                <small>(e.g. Biology, BIOL)</small>
+              </p>
+              <p>
+                Begin exploring the Prereq Map by choosing a curriculum
+                (subjects comprising a discipline, e.g., Biology or BIOL)
+                you’re interested in. Follow the lines to view prerequisites
+                for courses. View courses that have prereqs and courses that
+                are prereqs for other courses.
+              </p>
+            </div>
+          </b-carousel-slide>
+
+          <!-- Slides with custom text -->
+          <b-carousel-slide img-src="/static/prereq_map/img/modal-bg.jpg" img-alt="fsdaasdf">
+            <div class="text-left text-dark">
+              <p class="mt-4 mb-4 h3">
+                Get additional info about a course
+              </p>
+              <p>
+                Each square on the map represents a specific course. Click
+                the square to view prerequisite information for the course.
+                Click the links in the sidebar to learn more.
+              </p>
+            </div>
+          </b-carousel-slide>
+
+          <!-- Slide with blank fluid image to maintain slide aspect ratio -->
+          <b-carousel-slide img-src="/static/prereq_map/img/modal-bg.jpg" img-alt="asdfad">
+            <div class="text-left text-dark">
+              <p class="mt-4 mb-4 h3">
+                Get more course detail and find related curricula
+              </p>
+              <p>
+                On the Course Search page, click links to browse related
+                course offerings. You can also explore related curricula and
+                their prerequisite maps.
+              </p>
+
+              <p class="h6">
+                <strong>Important notes:</strong>
+              </p>
+
+              <ul>
+                <li>
+                  Prereq Map shows current prerequisites. When planning
+                  several quarters out, keep in mind that prerequisites may
+                  change over time.
+                </li>
+                <li>
+                  Prereq Map is based solely on course prerequisites and
+                  does not take into account specific requirements for
+                  graduation.
+                </li>
+                <li>
+                  Not all equivalencies (e.g. placement tests) are
+                  represented in the map – just courses. You can find some
+                  equivalencies in the text description.
+                </li>
+                <li>
+                  Remember to talk to your adviser when course planning.
+                </li>
+              </ul>
+              <div class="text-center mt-3">
+                <div>
+                  <a
+                    @click="
+                      $ga.event(
+                        'outbound',
+                        'click',
+                        'https://itconnect.uw.edu/learn/tools/prereq-map/'
+                      )
+                    "
+                    @keydown="
+                      $ga.event(
+                        'outbound',
+                        'click',
+                        'https://itconnect.uw.edu/learn/tools/prereq-map/'
+                      )
+                    "
+                    href="https://itconnect.uw.edu/learn/tools/prereq-map/"
+                    target="_blank"
+                  >
+                    Additional details
+                  </a>
+                </div>
+              </div>
+              <div class="text-center mt-2">
+                <button
+                  @click="accept"
+                  @keydown="accept"
+                  class="btn btn-primary prereq-purple"
+                >
+                  Got it!
+                </button>
+              </div>
+            </div>
+          </b-carousel-slide>
+        </b-carousel>
+      </div>
+    </b-modal>
+  </div>
+
   <!-- Modal -->
+  <!--
   <div
     id="onboardingModal"
     class="modal fade"
@@ -202,6 +342,9 @@
       </div>
     </div>
   </div>
+  -->
+
+
 </template>
 
 <script>
@@ -214,12 +357,20 @@
       return {};
     },
     mounted() {
+
+      
+
       // check if valid cookie exists and user has not yet accepted terms
       if (this.$cookies.get("prereq-onboarding-accepted") == "false") {
+
         // show the onboarding modal
+        
+        /*
         $("#onboardingModal").modal({ backdrop: "static", keyboard: false });
-        //
         this.carouselConfig();
+        */
+
+        this.$bvModal.show('bv-modal-example');
 
         // google analytics (vue syntax)
         this.$ga.page({
@@ -248,6 +399,7 @@
         });
       },
 
+      /*
       carouselConfig: function() {
         var carouselLength = $(".carousel-item").length - 1;
         // If there is more than one item
@@ -300,12 +452,33 @@
             }
           });
       }
+      */ 
+     
     }
   };
 </script>
 
 <style lang="scss">
   @import '../../css/_mixins.scss';
+
+  .modal-backdrop {
+    opacity: 0.8;
+  }
+
+  .carousel-item {
+    background: #fff !important;
+  }
+
+  .carousel-control-prev-icon {
+    background-image: url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%23000' viewBox='0 0 8 8'%3E%3Cpath d='M5.25 0l-4 4 4 4 1.5-1.5-2.5-2.5 2.5-2.5-1.5-1.5z'/%3E%3C/svg%3E");
+  }
+
+  .carousel-control-next-icon {
+    background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' fill='%23000' viewBox='0 0 8 8'%3e%3cpath d='M2.75 0l-1.5 1.5 2.5 2.5-2.5 2.5 1.5 1.5 4-4-4-4z'/%3e%3c/svg%3e");
+  }
+
+
+  /*
 
   .modal-backdrop {
     opacity: 0.8 !important;
@@ -377,4 +550,5 @@
     background-color: #4d307f;
     border-color: #4d307f;
   }
+  */
 </style>
