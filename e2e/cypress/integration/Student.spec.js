682ac5aa2aba5282f9e5e4578cdad3cbb06b7901
Charlon Palacay <charlon@gmail.com>
2019-09-20 12:04:12 -0700

    Updates cypress test.

--- e2e/cypress/integration/Student.spec.js
+++ e2e/cypress/integration/Student.spec.js
@@ -1,12 +1,13 @@
-describe('Demo Test', function() {
-  it('t1', function() {
-    cy.visit('')
-    cy.title().should('eq', 'Prereq Map - University of Washington')
-    cy.wait(500)
-    cy.document().toMatchImageSnapshot()
-
-    cy.get('.btn.btn-primary.float-right').click()
-    cy.wait(100)
-    cy.document().toMatchImageSnapshot()
+describe('Search Page', function() {
+  it('Displays accept modal', function() {
+    cy.visit('');
+    cy.title().should('eq', 'Prereq Map - University of Washington');
+    cy.wait(500);
+    cy.document().toMatchImageSnapshot();
+  })
+  it('Removes modal when user accepts', function() {
+    cy.get('.btn.btn-primary.float-right').click();
+    cy.wait(100);
+    cy.document().toMatchImageSnapshot();
   })
 })
