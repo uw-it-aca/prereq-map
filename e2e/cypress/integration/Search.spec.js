5670435e691a8b5c453938e76dba5a1f1dc81f16
Sarthak Singh <ss269@uw.edu>
2019-09-20 12:55:32 -0700

    Changed snapshot plugin

--- e2e/cypress/integration/Search.spec.js
+++ e2e/cypress/integration/Search.spec.js
@@ -3,11 +3,11 @@ describe('Search Page', function() {
     cy.visit('');
     cy.title().should('eq', 'Prereq Map - University of Washington');
     cy.wait(500);
-    cy.document().toMatchImageSnapshot();
+    cy.document().matchImageSnapshot();
   })
   it('Removes modal when user accepts', function() {
     cy.get('.btn.btn-primary.float-right').click();
     cy.wait(100);
-    cy.document().toMatchImageSnapshot();
+    cy.document().matchImageSnapshot();
   })
 })
--- e2e/cypress/support/commands.js
+++ e2e/cypress/support/commands.js
@@ -24,4 +24,6 @@
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
-import 'cypress-plugin-snapshots/commands';
+import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
+
+addMatchImageSnapshotCommand();
