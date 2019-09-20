4ca0833b96aed06defc2be4b27a61048165b66ca
Charlon Palacay <charlon@gmail.com>
2019-09-20 15:21:11 -0700

    Fixes diffed snapshot and about prereq map link in header.

--- e2e/cypress/integration/Search.spec.js
+++ e2e/cypress/integration/Search.spec.js
@@ -1,13 +1,26 @@
 describe('Search Page', function() {
+
   it('Displays accept modal', function() {
-    cy.visit('');
-    cy.title().should('eq', 'Prereq Map - University of Washington');
+    cy.visit('/');
     cy.wait(500);
+    // take a snapshot
     cy.document().matchImageSnapshot();
   })
-  it('Removes modal when user accepts', function() {
+
+  it('Displays Search page when user accepts', function() {
+
+    // click the accept
     cy.get('.btn.btn-primary.float-right').click();
+
+    // Course radio button should be selected
+    cy.get('[type="radio"]').first().should("be.checked");
+    // check for other stuff
+
+    // make sure Discover shows Bothell, Seattle, Tacoma
+
     cy.wait(100);
+
+    // take a snapshot of default state
     cy.document().matchImageSnapshot();
   })
 })
