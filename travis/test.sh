fd5e9809dc3015456a99ecf348b24ad4cd5736aa
mike seibel <mikes@washington.edu>
2019-12-12 13:55:56 -0800

    tests

--- travis/test.sh
+++ travis/test.sh
@@ -13,7 +13,7 @@ source bin/activate
 # install test tooling
 pip install pycodestyle coverage
 apt-get install -y nodejs npm gcc-4.8 unixodbc-dev
-npm install -g stylelint eslint
+npm install
 
 function run_test {
     echo "##########################"
@@ -22,9 +22,9 @@ function run_test {
 }
 
 run_test "pycodestyle ${DJANGO_APP}/ --exclude=migrations,static"
-run_test "stylelint '**/*.vue' '**/*.css' '**/*.scss'"
-run_test "eslint --ext .js,.vue prereq_map/static/prereq_map/js/components/"
-run_test "eslint --ext .js,.vue prereq_map/static/prereq_map/js/pages/"
+run_test "./node_modules/.bin/stylelint '**/*.vue' '**/*.css' '**/*.scss'"
+run_test "./node_modules/.bin/eslint --ext .js,.vue prereq_map/static/prereq_map/js/components/"
+run_test "./node_modules/.bin/eslint --ext .js,.vue prereq_map/static/prereq_map/js/pages/"
 run_test "coverage run --source=${DJANGO_APP} '--omit=*/migrations/*' manage.py test ${DJANGO_APP}"
 
 # put generaged coverage result where it will get processed
