51a5470778357482fe563a4c0d35e9e51c7f0b1b
mike seibel <mikes@washington.edu>
2019-12-12 13:25:25 -0800

    stylelint, test-values

--- travis/test.sh
+++ travis/test.sh
@@ -13,7 +13,7 @@ source bin/activate
 # install test tooling
 pip install pycodestyle coverage
 apt-get install -y nodejs npm gcc-4.8 unixodbc-dev
-npm install -g eslint
+npm install -g stylelint eslint
 
 function run_test {
     echo "##########################"
