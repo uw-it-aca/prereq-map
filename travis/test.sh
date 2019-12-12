98b8a2e47069f168b3c8551d8f971eb02a46e019
mike seibel <mikes@washington.edu>
2019-12-11 16:21:37 -0800

    travis tests in docker/test.sh

---
+++ travis/test.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+set -e
+trap 'exit 1' ERR
+
+# travis test script for django app
+#
+# PRECONDITION: inherited env vars from application's .travis.yml MUST include:
+#      DJANGO_APP: django application directory name
+
+# start virtualenv
+source bin/activate
+
+# install test tooling
+pip install pycodestyle coverage
+apt-get install -y nodejs npm gcc-4.8 unixodbc-dev
+npm install -g eslint
+
+function run_test {
+    echo "##########################"
+    echo "TEST: $1"
+    eval $1
+}
+
+run_test "pycodestyle ${DJANGO_APP}/ --exclude=migrations,static"
+run_test "stylelint '**/*.vue' '**/*.css' '**/*.scss'"
+run_test "eslint --ext .js,.vue prereq_map/static/prereq_map/js/components/"
+run_test "eslint --ext .js,.vue prereq_map/static/prereq_map/js/pages/"
+run_test "coverage run --source=${DJANGO_APP} '--omit=*/migrations/*' manage.py test ${DJANGO_APP}"
+
+# put generaged coverage result where it will get processed
+cp .coverage.* /coverage
+
+exit 0
