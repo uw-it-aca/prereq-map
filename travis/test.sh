d0c22d0b3ed791e81f7b20ba87486f7cf453a52d
mike seibel <mikes@washington.edu>
2019-12-12 16:05:47 -0800

    stylelint

--- travis/test.sh
+++ travis/test.sh
@@ -22,7 +22,7 @@ function run_test {
 }
 
 run_test "pycodestyle ${DJANGO_APP}/ --exclude=migrations,static"
-run_test "./node_modules/.bin/stylelint '**/*.vue' '**/*.css' '**/*.scss'"
+run_test "./node_modules/.bin/stylelint 'prereq_map/**/*.vue' 'prereq_map/**/*.css' 'prereq_map/**/*.scss'"
 run_test "./node_modules/.bin/eslint --ext .js,.vue prereq_map/static/prereq_map/js/components/"
 run_test "./node_modules/.bin/eslint --ext .js,.vue prereq_map/static/prereq_map/js/pages/"
 run_test "coverage run --source=${DJANGO_APP} '--omit=*/migrations/*' manage.py test ${DJANGO_APP}"
