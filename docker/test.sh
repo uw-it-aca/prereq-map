caefe1213b664e7162b7a3851db6a428eeeac9af
Jim Laney <jlaney@uw.edu>
2021-03-31 15:38:21 -0700

    use installed test tooling

--- docker/test.sh
+++ docker/test.sh
@@ -15,18 +15,6 @@ function run_test {
 
 run_test "pycodestyle ${DJANGO_APP}/ --exclude=migrations,static"
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-nvm install 14.15
-nvm use node
-node -v
-
-npm install -g eslint@7.0.0 stylelint@13.3.3 eslint-plugin-vue@latest
-npm install
-
 run_test "eslint --ext .js,.vue ${DJANGO_APP}/static/${DJANGO_APP}/js/components/"
 run_test "eslint --ext .js,.vue ${DJANGO_APP}/static/${DJANGO_APP}/js/pages/"
 run_test "stylelint '${DJANGO_APP}/**/*.vue' '${DJANGO_APP}/**/*.css' '${DJANGO_APP}/**/*.scss'"
