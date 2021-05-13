8d7a16575e4203979367a589e2710524ed1c8615
Stephen De Vight <devights@uw.edu>
2021-05-12 19:03:34 -0700

    using prior pickle version

--- prereq_map/management/commands/fetch_data.py
+++ prereq_map/management/commands/fetch_data.py
@@ -37,5 +37,5 @@ class Command(BaseCommand):
         CurricTitles.update_titles(curric_info, course_info)
         CourseTitle.update_titles(course_info)
 
-        prereq.to_pickle(os.path.join(data_root, "prereq_data.pkl"))
-        course_info.to_pickle(os.path.join(data_root, "course_data.pkl"))
+        prereq.to_pickle(os.path.join(data_root, "prereq_data.pkl"), protocol=4)
+        course_info.to_pickle(os.path.join(data_root, "course_data.pkl"), protocol=4)
